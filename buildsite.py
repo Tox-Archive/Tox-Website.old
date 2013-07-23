3e17e1128eab98340a134752107b3c671e92e3c6
Sebastian Stal <stal@pyboard.net>
2013-07-22 18:39:49 -0700

    Fix buildsite again for python 3

--- buildsite.py
+++ buildsite.py
@@ -57,7 +57,10 @@ def main():
     for template_name in templates:
         with open(template_name) as tmp_file:
             print("Building template {0}.".format(template_name))
-            do_format_template(template_name, tmp_file.read().decode("utf-8"))
+            if sys.version_info.major == 2:
+                do_format_template(template_name, tmp_file.read().decode("utf-8"))
+            else:
+                do_format_template(template_name, tmp_file.read())
 
 if __name__ == "__main__":
     main()
